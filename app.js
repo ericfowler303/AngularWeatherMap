9239c346de306569189d106b03b25b11c3bf7fa7
Eric Fowler <ericfowler303@gmail.com>
2014-12-17 16:15:30 -0700

    Complete address lookup and map location changing

--- app.js
+++ app.js
@@ -31,21 +31,25 @@ app.controller("LayersSimpleController", [ '$scope', function($scope) {
             scrollWheelZoom: false
         }
     });
+
+    $scope.setLocation = function(lat,lon) {
+        //alert(lat + ":" + lon);
+        this.denver.lat = parseFloat(lat);
+        this.denver.lng = parseFloat(lon);
+        this.denver.zoom = 7;
+    };
 } ]);
 
 app.controller('FormController', ['$scope', '$http', function($scope,$http){
     this.locationInput = "";
-    this.locationOutput =[];
-
+    
     $scope.updateLocation = function () {
         // preform a http.get to grab the new location
         var replacedInput = this.locationInput.replace(/ /g, '+');
-        var url ="http://nominatim.openstreetmap.org/search?q="+replacedInput+"&format=json&polygon=0&addressdetails=0"
+        var url ="http://nominatim.openstreetmap.org/search?q="+replacedInput+"&format=json&polygon=0&addressdetails=0&limit=1"
         
         $http.get(url).success( function(data) {
-            alert(url);
-            this.locationOutput = data;
-            alert(this.locationOutput);
+            $scope.setLocation(data[0].lat,data[0].lon);
         });
         
     };
