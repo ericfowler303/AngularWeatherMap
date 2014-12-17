9e3e369b7078d2ea194b404c01ed3656c5c8058d
Eric Fowler <ericfowler303@gmail.com>
2014-12-16 21:49:53 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+var app = angular.module('weather', ["leaflet-directive"]);
+
+app.controller("LayersSimpleController", [ '$scope', function($scope) {
+    angular.extend($scope, {
+        denver: {
+            lat: 39.73915,
+            lng: -104.9847,
+            zoom: 6
+        },
+        layers: {
+            baselayers: {
+                osm: {
+                    name: 'OpenStreetMap',
+                    url: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                    type: 'xyz'
+                }
+            }
+        },
+        defaults: {
+            scrollWheelZoom: false
+        }
+    });
+} ]);
